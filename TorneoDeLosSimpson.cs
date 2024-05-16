62f3e108454fe5557ee3e18773ed621c4d5619bf
kevinBedoya888 <kevinalexbb25@gmail.com>
2024-05-16 15:42:11 -0500

    Torneo de los Simpsons
    Torneo de los Simpsons

---
+++ TorneoDeLosSimpson.cs
@@ -0,0 +1,26 @@
+/******************************************************************************
+Autores: Kevin Bedoya, Justin Muñoz
+Fecha: 16/05/2024
+los Simpson estan organizando un torneo de videojuegos y necesitan un programa
+C# para mostrar los nombres de los participantes. Completa el espacio en blancl para
+imprimir los nombres em la consola:
+*******************************************************************************/
+using System;
+class oliii {
+  static void Main(string[] args) {
+  
+  string participante1 = "Bart";
+  string participante2 = "Lisa";
+  string participante3 = "Milhouse";
+  string participante4 = "Nelson";
+  
+  Console.WriteLine("Participante 1: " + participante1);
+  Console.WriteLine("Participante 2: " + participante2);
+  Console.WriteLine("Participante 3: " + participante3);
+  Console.WriteLine("Participante 3: " + participante4);
+  
+
+      
+  }
+  
+}
