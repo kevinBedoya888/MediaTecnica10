c747ddedad18b15d8f4f2c0c55360beb670408b0
kevinBedoya888 <kevinalexbb25@gmail.com>
2024-05-16 15:41:04 -0500

    Stranger Thing
    Stranger Thing

---
+++ Personajes de Stranger Things.cs
@@ -0,0 +1,19 @@
+/******************************************************************************
+Autores: Kevin Bedoya, Justin Muñoz
+Fecha: 16/05/2024
+Los personajes de Stranger Things necesitan un prgrama en C# para registrar
+los encuentros con ciraturas del mundo como el Demogorgon. Completa el espacio
+en blanco para imprimir el nombre de la criatura en la consola
+
+*******************************************************************************/
+using System;
+class oliii {
+  static void Main(string[] args) {
+  
+  string nombreCriatura = "Demogorgon";
+  Console.WriteLine("Nombre de la criatura: " + nombreCriatura);
+
+      
+  }
+  
+}
